0833ea47fcc49885bd861c9a63d6ad3404043731
Mateus Garcia <mateuspgx5@live.com>
2017-10-28 04:41:37 -0300

    Novos

--- js/script.js
+++ js/script.js
@@ -24,4 +24,19 @@ $(function(){
 		} 
 	});  
 });		
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('a[href="#top"]').fadeIn();
+        } else {
+            $('a[href="#top"]').fadeOut();
+        }
+    });
+
+    $('a[href="#top"]').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
+
 

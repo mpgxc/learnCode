6f5dc3b8f7f53890841b65ff1f2d5693a2ddd919
Mateus Garcia <mateuspgx5@live.com>
2017-11-15 17:44:25 -0300

    Slideshow

---
+++ js/Cookies.js
@@ -0,0 +1,35 @@
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Bem-Vindo de volta " + user + " ಠᴗಠ");
+    } else {
+       user = prompt("Coloque seu nome de jovem gafanhoto ¯\\_(ツ)_/¯ ","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+       alert("Bem-Vindo "+user);
+    }
+}

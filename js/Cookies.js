2080ac802268fdc2819669168c7baaae09590949
mateuspgx5 <mateuspgx5@live.com>
2017-11-28 04:53:50 -0300

    AS

--- js/Cookies.js
+++ js/Cookies.js
@@ -2,7 +2,7 @@ function setCookie(cname,cvalue,exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
-    if(cname == ""){
+    if(cname == "" || cname == null){
       document.getElementById("Aluno").innerHTML = "Anônimo";
     }else{
           document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";  
